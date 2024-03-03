da73f68382797caa1cdc98d6fa40f06910464c7d
Viet Nguyen <nghoangviet.english@gmail.com>
2024-03-03 17:21:21 +1000

    gradually check

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -9,14 +9,14 @@ def health():
 @api.route('/todos', methods=['GET'])
 def get_todos():
     return jsonify([{
-    "id": 1,
-    "title": "Watch CSSE6400 Lecture",
-    "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-    "completed": True,
-    "deadline_at": "2023-02-27T00:00:00",
-    "created_at": "2023-02-20T00:00:00",
-    "updated_at": "2023-02-20T00:00:00"
-    }])
+        "id": 1,
+        "title": "Watch CSSE6400 Lecture",
+        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
+        "completed": True,
+        "deadline_at": "2023-02-27T00:00:00",
+        "created_at": "2023-02-20T00:00:00",
+        "updated_at": "2023-02-20T00:00:00"
+ }])
 
 @api.route('/todos/<int:id>', methods=['GET'])
 def get_todo(id):
@@ -33,13 +33,13 @@ def get_todo(id):
 @api.route('/todos', methods=['POST'])
 def create_todo():
     return jsonify({
-    "id": 1,
-    "title": "Watch CSSE6400 Lecture",
-    "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-    "completed": True,
-    "deadline_at": "2023-02-27T00:00:00",
-    "created_at": "2023-02-20T00:00:00",
-    "updated_at": "2023-02-20T00:00:00"
+        "id": 1,
+        "title": "Watch CSSE6400 Lecture",
+        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
+        "completed": True,
+        "deadline_at": "2023-02-27T00:00:00",
+        "created_at": "2023-02-20T00:00:00",
+        "updated_at": "2023-02-20T00:00:00"
     }), 201
 
 @api.route('/todos/<int:id>', methods=['PUT'])
@@ -57,11 +57,11 @@ def update_todo(id):
 @api.route('/todos/<int:id>', methods=['DELETE'])
 def delete_todo(id):
     return jsonify({
-    "id": id,
-    "title": "Watch CSSE6400 Lecture",
-    "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-    "completed": True,
-    "deadline_at": "2023-02-27T00:00:00",
-    "created_at": "2023-02-20T00:00:00",
-    "updated_at": "2023-02-20T00:00:00"
-    })
+        "id": id,
+        "title": "Watch CSSE6400 Lecture",
+        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
+        "completed": True,
+        "deadline_at": "2023-02-27T00:00:00",
+        "created_at": "2023-02-20T00:00:00",
+        "updated_at": "2023-02-20T00:00:00"
+    })    
